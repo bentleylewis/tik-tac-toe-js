d782a49fadd89ad5c80f8b4eb482351d94f39017
bentleylewis <bbentleylewis@gmail.com>
2025-08-17 17:02:11 -0500

    redesign color palette and layout. add to cycle game functionality.

--- main.js
+++ main.js
@@ -2,16 +2,20 @@
 //TODO: maybe we can add user choices for pfp of funny cat images ?? 
 const boardDiv = document.getElementById("board");
 const turnDiv = document.getElementById("turn");
+const scoreOneDiv = document.getElementById("user-one-score");
+const scoreTwoDiv = document.getElementById("user-two-score");
+const playAgainButton = document.getElementById("play-again-button");
 
-// Use whosThatCatSrc if you're feeling goofy. 
+let gameActive = true;
 
 const whosThatCatSrc = 'Images/starting-cell-div.jpg';
 const startImage = 'Images/loaf-cat.jpg';
 const userOneImageSrc = 'Images/cat-user-1.jpg';
 const userTwoImageSrc = 'Images/cat-user-2.jpg';
+const drawCatImageSrc = 'Images/draw-cat.jpg';
 
-const userOne = createUser("playerOne", 'x', userOneImageSrc);
-const userTwo = createUser('playerTwo', 'o', userTwoImageSrc);
+const userOne = createUser("eepy kitty", 'x', userOneImageSrc);
+const userTwo = createUser('goofy kitty', 'o', userTwoImageSrc);
 
 boardDiv.textContent = "";
 
@@ -35,7 +39,6 @@ const gameboard = (()  => {
             return grid[r][c];
         },
         // set cell if no value
-        // TODO: add checking wherever we are calling this method, will likely be a comparison after out getCell
         setCell: (r, c, symbol) => {
             grid[r][c] = symbol;
 
@@ -58,7 +61,8 @@ function createUser (name, symbol, image) {
     return {
         name, 
         symbol,
-        image, 
+        image,
+        score: 0,
         isTurn: false,
     }
 }
@@ -68,10 +72,6 @@ function createUser (name, symbol, image) {
 const gameController = (function () {
     let currentPlayer = userOne;
     return {
-
-        start: () => {
-            gameboard.reset();
-        },
 
         switchPlayer: () => {
             currentPlayer = (currentPlayer === userOne) ? userTwo : userOne;
@@ -87,30 +87,6 @@ const gameController = (function () {
             };
         },
 
-        checkWin: function (grid) {
-            const winConditions = [
-               [0, 1, 2], [3, 4, 5], [6, 7, 8],
-                
-               [0, 3, 6], [1, 4, 7], [2, 5, 8],
-    
-               [0, 4, 8], [2, 4, 6]
-                ];
-            // Loop through each element in the winConditions array, and assign each one in turn to the variable condition (for... of loop)
-            const flatGrid = grid.flat();
-            for (let conditions of winConditions) {
-                const [a, b, c] = conditions;
-                if (
-                    flatGrid[a] === currentPlayer.symbol &&
-                    flatGrid[b] === currentPlayer.symbol &&
-                    flatGrid[c] === currentPlayer.symbol
-                ) {
-                    console.log(`${currentPlayer.name} wins!`);
-                    return;
-                }
-            }
-
-        },
-
         writeToGrid: function(rowIndex, columnIndex, gridImage) {
             gridImage.classList.remove('startingImage');
             gameboard.setCell(rowIndex, columnIndex, currentPlayer.symbol);
@@ -119,11 +95,88 @@ const gameController = (function () {
 
         },
 
+        checkWin: function (flatGrid) {
+            const winConditions = [
+               [0, 1, 2], [3, 4, 5], [6, 7, 8],
+                
+               [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    
+               [0, 4, 8], [2, 4, 6]
+                ];
+            // Loop through each element in the winConditions array, and assign each one in turn to the variable condition (for... of loop)
+            for (let conditions of winConditions) {
+                const [a, b, c] = conditions;
+                if (
+                    flatGrid[a] === currentPlayer.symbol &&
+                    flatGrid[b] === currentPlayer.symbol &&
+                    flatGrid[c] === currentPlayer.symbol
+                ) {
+                    return true; 
+                }
+            }
+            return false; 
+
+        },
+        checkDraw: function (flatGrid) {
+            for (let i = 0; i < flatGrid.length; i++) {
+                if (flatGrid[i] === '') {
+                    return false;
+                }
+            }
+            return true;
+        },
+
+        checkGameOver: function (grid) {
+            const flatGrid = grid.flat();
+            if (this.checkWin(flatGrid) === true) {
+                return 'win';
+            }
+            else if (this.checkDraw(flatGrid) === true) {
+                return 'draw';
+            }
+            return 'continue';
+    },
+
         resetGame: function () {
+            currentPlayer = userOne;
+            turnDiv.innerHTML =`<div class='score-html-container'>It is ${currentPlayer.name}'s turn, Begin!<img class="scoreImage"></img></div>`;
+            const turnImg = turnDiv.querySelector('img');
+            turnImg.src = currentPlayer.image;
+            gameActive = true;
             gameboard.reset();
 
         },
 
+        playTurn: function (rowIndex, columnIndex, gridImage) {
+            if (gameActive === false) {
+                return;
+            }
+            else {
+            if(gameController.isAvailable(rowIndex, columnIndex) === true) {
+            gameController.writeToGrid(rowIndex, columnIndex, gridImage);
+            let result = gameController.checkGameOver(gameboard.getGrid());
+            if (result === 'win') {
+                turnDiv.innerHTML =`<div class='score-html-container'>${currentPlayer.name} Wins! <img class="scoreImage"></img></div>`;
+                const winImg = turnDiv.querySelector('img');
+                winImg.src = currentPlayer.image;
+                gameActive = false;
+            }
+            else if (result === 'draw') {
+                turnDiv.innerHTML = `<div class='score-html-container'>It's a Draw...<img class="scoreImage"></img></div>`;
+                const DrawImg = turnDiv.querySelector('img');
+                DrawImg.src = drawCatImageSrc;
+                gameActive = false;
+            } 
+            else {
+            gameController.switchPlayer();
+            turnDiv.innerHTML =`<div class='score-html-container'>It is ${currentPlayer.name}'s turn!<img class="scoreImage"></img></div>`;
+            const turnImg = turnDiv.querySelector('img');
+            turnImg.src = currentPlayer.image;
+                }
+            
+            }
+        }
+        }
     }
 })();
 
@@ -144,17 +197,19 @@ const displayController = (function () {
          cellDiv.addEventListener("click", () => {
             const rowIndex = Number(cellDiv.dataset.row); 
             const columnIndex = Number(cellDiv.dataset.column);
-
-            if(gameController.isAvailable(rowIndex, columnIndex) === true) {
-            gameController.writeToGrid(rowIndex, columnIndex, gridImage);
-            gameController.checkWin(gameboard.getGrid());
-            gameController.switchPlayer();
-            }
+            gameController.playTurn(rowIndex, columnIndex, gridImage);
             console.log(gameboard.getGrid());
          });
          cellDiv.appendChild(gridImage);
          boardDiv.appendChild(cellDiv);
        }
     }
-//TODO: maybe here we can make the box outline or something for when isAvailable returns false
+    playAgainButton.addEventListener("click", () => {
+        const collectedCells = boardDiv.querySelectorAll(".gridImage");
+        collectedCells.forEach((image) => {
+            image.classList.add('startingImage');
+            image.src = startImage;
+        });
+        gameController.resetGame();
+    })
 })();
